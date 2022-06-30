7dc726f02b8b866bdac5a97a714c2bdb4e2c1ec8
Antonio Sarosi <sarosiantonio@gmail.com>
2022-06-30 22:57:51 +0200

    Number rules

--- tests/Validation/ValidationRulesTest.php
+++ tests/Validation/ValidationRulesTest.php
@@ -3,7 +3,10 @@
 namespace Lune\Tests\Validation;
 
 use Lune\Validation\Rules\Email;
+use Lune\Validation\Rules\LessThan;
+use Lune\Validation\Rules\Number;
 use Lune\Validation\Rules\Required;
+use Lune\Validation\Rules\RequiredWhen;
 use Lune\Validation\Rules\RequiredWith;
 use PHPUnit\Framework\TestCase;
 
@@ -57,5 +60,73 @@ class ValidationRulesTest extends TestCase {
         $this->assertTrue($rule->isValid('test', $data));
         $data = ['other' => 10];
         $this->assertFalse($rule->isValid('test', $data));
+    }
+
+    public function lessThanData() {
+        return [
+            [5, 5, false],
+            [5, 6, false],
+            [5, 3, true],
+            [5, null, false],
+            [5, "", false],
+            [5, "test", false],
+        ];
+    }
+
+    /**
+     * @dataProvider lessThanData
+     */
+    public function test_less_than($value, $check, $expected) {
+        $rule = new LessThan($value);
+        $data = ["test" => $check];
+        $this->assertEquals($expected, $rule->isValid("test", $data));
+    }
+
+    public function numbers() {
+        return [
+            [0, true],
+            [1, true],
+            [1.5, true],
+            [-1, true],
+            [-1.5, true],
+            ["0", true],
+            ["1", true],
+            ["1.5", true],
+            ["-1", true],
+            ["-1.5", true],
+            ["test", false],
+            ["1test", false],
+            ["-5test", false],
+            ["", false],
+            [null, false],
+        ];
+    }
+
+    /**
+     * @dataProvider numbers
+     */
+    public function test_number($n, $expected) {
+        $rule = new Number();
+        $data = ["test" => $n];
+        $this->assertEquals($expected, $rule->isValid("test", $data));
+    }
+
+    public function requiredWhenData() {
+        return [
+            ["other", "=", "value", ["other" => "value"], "test", false],
+            ["other", "=", "value", ["other" => "value", "test" => 1], "test", true],
+            ["other", "=", "value", ["other" => "not value"], "test", true],
+            ["other", ">", 5, ["other" => 1], "test", true],
+            ["other", ">", 5, ["other" => 6], "test", false],
+            ["other", ">", 5, ["other" => 6, "test" => 1], "test", true],
+        ];
+    }
+
+    /**
+     * @dataProvider requiredWhenData
+     */
+    public function test_required_when($other, $operator, $compareWith, $data, $field, $expected) {
+        $rule = new RequiredWhen($other, $operator, $compareWith);
+        $this->assertEquals($expected, $rule->isValid($field, $data));
     }
 }
